071259a4748e83d3bf885462af13206634086718
Artemii Oliinyk <aoliinyk@n-ix.com>
2022-04-20 21:06:58 +0300

    Create photo_item.dart

---
+++ lib/photo_item.dart
@@ -0,0 +1,20 @@
+class PhotoItem {
+  final String name;
+  final String authorName;
+  final String urlSmallImage;
+  final String urlBigImage;
+
+  PhotoItem({
+    required this.name,
+    required this.authorName,
+    required this.urlSmallImage,
+    required this.urlBigImage,
+  });
+
+  factory PhotoItem.fromJson(Map<String, dynamic> json) => PhotoItem(
+    name: json['alt_description'],
+    authorName: json['user']['name'],
+    urlSmallImage: json["urls"]['small'],
+    urlBigImage: json["urls"]['small'],
+  );
+}
